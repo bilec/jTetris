8d68a79a5205b91e3a95431e9ecd76cf925a46a9
bilec <martin.bilka826@gmail.com>
2022-10-31 09:20:04 +0100

    feat: gameFrame implemented

--- src/main/kotlin/gui/frame/GameFrame.kt
+++ src/main/kotlin/gui/frame/GameFrame.kt
@@ -1,6 +1,32 @@
 package gui.frame
 
+import gui.label.InfoLabel
+import gui.menubar.GameMenuBar
+import gui.panel.GamePanel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import utils.config.Config
+import java.awt.BorderLayout
+import java.awt.Dimension
 import javax.swing.JFrame
 
-class GameFrame : JFrame() {
+class GameFrame: JFrame(), KoinComponent {
+    private val config: Config by inject()
+    private val infoLabel: InfoLabel by inject()
+    private val gamePanel: GamePanel by inject()
+    private val gameMenuBar: GameMenuBar by inject()
+
+    init {
+        title = config.appName
+        defaultCloseOperation = EXIT_ON_CLOSE
+
+        size = Dimension(config.gameWindowSize.width, config.gameWindowSize.height)
+        isResizable = false
+
+        jMenuBar = gameMenuBar
+        add(gamePanel)
+        add(infoLabel, BorderLayout.SOUTH)
+
+        isVisible = true
+    }
 }
