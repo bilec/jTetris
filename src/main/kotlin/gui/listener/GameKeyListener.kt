d2b0fbf045a46e00e60b00c3a4d462c578feda6e
bilec <martin.bilka826@gmail.com>
2022-10-31 09:24:12 +0100

    feat: gameKeyListener implemented

--- src/main/kotlin/gui/listener/GameKeyListener.kt
+++ src/main/kotlin/gui/listener/GameKeyListener.kt
@@ -1,18 +1,62 @@
 package gui.listener
 
+import game.Game
+import game.isTetrominoMovableToLeft
+import game.isTetrominoMovableToRight
+import game.isTetrominoRotatable
+import gui.label.InfoLabel
+import gui.panel.GamePanel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import utils.config.NumberOfPossibleRotations
+import utils.config.TetrominoSpeed
 import java.awt.event.KeyEvent
 import java.awt.event.KeyListener
 
-class GameKeyListener : KeyListener {
-    override fun keyTyped(e: KeyEvent?) {
-        TODO("Not yet implemented")
-    }
+class GameKeyListener : KeyListener, KoinComponent {
+    private val tetrominoSpeed: TetrominoSpeed by inject()
+    private val numberOfPossibleRotations: NumberOfPossibleRotations by inject()
+
+    private val gamePanel: GamePanel by inject()
+    private val infoLabel: InfoLabel by inject()
+
+    override fun keyTyped(e: KeyEvent?) { /* NOT NEEDED */ }
 
     override fun keyPressed(e: KeyEvent?) {
-        TODO("Not yet implemented")
+        if (Game.isRunning && gamePanel.timer.isRunning) {
+            when (e?.keyCode) {
+                KeyEvent.VK_LEFT -> if (isTetrominoMovableToLeft(Game.currentTetromino, Game.gameBoard)) Game.currentTetromino.point.x--
+                KeyEvent.VK_RIGHT -> if (isTetrominoMovableToRight(Game.currentTetromino, Game.gameBoard)) Game.currentTetromino.point.x++
+                KeyEvent.VK_DOWN -> gamePanel.timer.delay = tetrominoSpeed.fast
+            }
+            gamePanel.repaint()
+        }
     }
 
     override fun keyReleased(e: KeyEvent?) {
-        TODO("Not yet implemented")
+        if (Game.isRunning) {
+            if (gamePanel.timer.isRunning) {
+                when (e?.keyCode) {
+                    KeyEvent.VK_UP -> {
+                        if (isTetrominoRotatable(Game.currentTetromino, Game.gameBoard)
+                            && Game.rotationsMade < numberOfPossibleRotations.value) {
+                            Game.currentTetromino.rotate()
+                            Game.rotationsMade++
+                        }
+                    }
+                    KeyEvent.VK_DOWN -> gamePanel.timer.delay = tetrominoSpeed.normal
+                }
+            }
+
+            if (e?.keyCode == KeyEvent.VK_P) if (gamePanel.timer.isRunning) {
+                gamePanel.timer.stop()
+                infoLabel.setPaused()
+            } else {
+                gamePanel.timer.start()
+                infoLabel.setScore(Game.score)
+            }
+
+            gamePanel.repaint()
+        }
     }
 }
