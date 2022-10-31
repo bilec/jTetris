f3062bab820ec6b2668440ac8b004954c4760e0f
bilec <martin.bilka826@gmail.com>
2022-10-31 09:04:28 +0100

    feat: infoLabel implemented

--- src/main/kotlin/gui/label/InfoLabel.kt
+++ src/main/kotlin/gui/label/InfoLabel.kt
@@ -3,4 +3,19 @@ package gui.label
 import javax.swing.JLabel
 
 class InfoLabel : JLabel() {
+    init {
+        text = "Game not started"
+    }
+
+    fun setScore(score: Int) {
+        text = "Score: $score"
+    }
+
+    fun setGameOver(score: Int) {
+        text = "Game over, score: $score"
+    }
+
+    fun setPaused() {
+        text = "Paused"
+    }
 }
